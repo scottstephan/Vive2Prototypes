29f9f575af86c0176741e6e4ce92b90b3ca776ea
Scott Stephan <scott.stephan@gmail.com>
2015-07-23 15:44:00 -0700

    Adjusted base controller scripts

--- Assets/scripts/svrInteractableBase.cs
+++ Assets/scripts/svrInteractableBase.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+//SHOULD REQUIRE RBODY etc
 public class svrInteractableBase : MonoBehaviour {
+
 	private controllerListener.svrController activatingController;
 	private Rigidbody thisRigidbody;
+	private Color originColor;
 
 	// Use this for initialization
-	void Start () {
-	
+	public virtual void Start () {
+		thisRigidbody = gameObject.GetComponent<Rigidbody>();
+		originColor = gameObject.GetComponent<MeshRenderer>().material.color;
+		//Instance the mat to avoid overwriting the original material. 
+		Material cloneMat = gameObject.GetComponent<MeshRenderer>().material;
+		gameObject.GetComponent<MeshRenderer>().material = cloneMat; 
 	}
 	
 	// Update is called once per frame
@@ -19,6 +25,7 @@ public class svrInteractableBase : MonoBehaviour {
 	{
 		Debug.Log(gameObject.name + "has heard the svrDown Broadcast");
 		activatingController = controllerThatBroadcasted;
+		Debug.Log ("It was touched by cotnroller" + activatingController.index);
 		controllerInputTriggerDown();
 
 	}
@@ -26,8 +33,6 @@ public class svrInteractableBase : MonoBehaviour {
 	private void svrControllerUp(controllerListener.svrController controllerThatBroadcasted)
 	{
 		Debug.Log(gameObject.name + "has heard the svr Up Broadcast");
-		activatingController = null;
-
 		controllerInputTriggerUp();
 	}
 
@@ -38,18 +43,18 @@ public class svrInteractableBase : MonoBehaviour {
 	public virtual void controllerInputTriggerUp(){
 		
 	}
+
 //BELOW THIS POINT: Common functions for SVR Interactable Objects
 	public void objectIsPickedUp() 
 	{
 		//     gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
-		//Maybe add a short delay for feeding?
-		StartCoroutine("feedTransformDelay");
 		thisRigidbody.isKinematic = true;
 		gameObject.transform.parent = activatingController.controllerObject.transform;
 	}
 	
 	public void objectIsDropped() 
 	{
+
 		gameObject.transform.parent = null;
 		
 		Vector3 controllerVelocity = activatingController.curVelocity;
--- Assets/scripts/testSVRInputListener.cs
+++ Assets/scripts/testSVRInputListener.cs
@@ -2,22 +2,15 @@
 using System.Collections;
 //This is the most basic version of an SVR interactable object
 public class testSVRInputListener : svrInteractableBase {
-    private Color originColor;
-    private Rigidbody thisRigidbody;
-   
+    
     void Start()
     {
-        thisRigidbody = gameObject.GetComponent<Rigidbody>();
-        originColor = gameObject.GetComponent<MeshRenderer>().material.color;
-            //Instance the mat to avoid overwriting the original material. 
-        Material cloneMat = gameObject.GetComponent<MeshRenderer>().material;
-        gameObject.GetComponent<MeshRenderer>().material = cloneMat; 
+		base.Start ();
     }
 
 	public override void controllerInputTriggerDown()
     {
 		base.controllerInputTriggerDown ();
-		Debug.Log(gameObject.name + "has heard the svrDown Broadcast");
 
 		objectIsPickedUp(); 
     }
@@ -25,7 +18,6 @@ public class testSVRInputListener : svrInteractableBase {
 	public override void controllerInputTriggerUp()
     {
 		base.controllerInputTriggerUp ();
-		Debug.Log(gameObject.name + "has heard the svUp Broadcast");
 
         objectIsDropped();
     }
